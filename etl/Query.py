db9721a3c77e69cd925a95c8b8f46aa4c457d6d0
Michael Lindner <Michael.Lindner@bush.edu>
2023-09-14 13:17:32 -0700

    Working SQL insertion

--- etl/Query.py
+++ etl/Query.py
@@ -1,10 +1,13 @@
 from BbApiConnector import BbApiConnector
 import os
+import psycopg2
+from psycopg2.extensions import AsIs
+from datetime import date
 
-print('hello world')
+print('Connecting to Blackbaud')
 
 #path = os.path.join(os.pardir, r'\config\app_secrets.json')
-path = r'C:\Users\Michael.Lindner\Documents\BlackbaudExtractor\config\app_secrets.json'
+path = r'C:\Users\Install\BlackbaudExtractor\config\app_secrets.json'
 
 api_conn = BbApiConnector(path)
 bb_session = api_conn.get_session()
@@ -46,7 +49,7 @@ bb_session = api_conn.get_session()
 #        "type": "object",
 #        "properties": {
 #          "id": {
-#            "type": "integer",
+#            "type": "integer",                           PRIMARY KEY
 #            "description": "The ID of the attendance",
 #            "format": "int64"
 #          },
@@ -139,7 +142,7 @@ bb_session = api_conn.get_session()
 #            "description": "The name of the student",
 #            "nullable": true
 #          },
-#          "student_user_id": {                                               PRIMARY KEY
+#          "student_user_id": {                                               FOREIGN KEY
 #            "type": "integer",
 #            "description": "The student user ID",
 #            "format": "int32"
@@ -160,18 +163,53 @@ bb_session = api_conn.get_session()
 #  "additionalProperties": false,
 #  "description": "A Collection"
 #}
-params = {
-    'level_id': 779,
-    'day': '3/14/2022',
-    'offering_type': 1
- }
-req = bb_session.get("https://api.sky.blackbaud.com/school/v1/attendance", params=params)
+print('Getting data')
 
-print(req.json()['value'][0])
-output = req.json()['value']
+# Level IDs: [{'id': 781, 'abbreviation': 'LS', 'name': 'Lower School'}, {'id': 780, 'abbreviation': 'MS', 'name': 'Middle School'}, {'id': 779, 'abbreviation': 'US', 'name': 'Upper School'}]
+# Offering types: [{'id': 0, 'description': 'School'}, {'id': 1, 'description': 'Academics'}, {'id': 2, 'description': 'Activities'}, {'id': 3, 'description': 'Advisory'}, {'id': 4, 'description': 'Dorms'}, {'id': 5, 'description': 'Transcript'}, {'id': 6, 'description': 'OfferedCourse'}, {'id': 8, 'description': 'SchoolWideAwards'}, {'id': 9, 'description': 'Athletic'}, {'id': 10, 'description': 'Admissions'}, {'id': 11, 'description': 'Community groups'}, {'id': 12, 'description': 'Mentor'}, {'id': 13, 'description': 'Events'}]
 
-for i in range(len(output)):
-    print(output[i]['student_user_id'])
+today = date.today()
+d1 = today.strftime('%m/%d/%Y')
 
+print('Connecting to postgres')
+conn = psycopg2.connect(
+        database='postgres',
+        user='postgres',
+        password='Kto12SQL',
+        host='localhost',
+        port='5432'
+)
 
+conn.autocommit = True
+cursor = conn.cursor()
+
+level_ids = ['781', '780', '779']
+for id in level_ids:
+    print(id)
+    
+    params = {
+        'level_id': id,
+        'day': d1,
+        'offering_type': 1
+    }
+    req = bb_session.get("https://api.sky.blackbaud.com/school/v1/attendance", params=params)
+
+    output = req.json()['value']
+
+    if (len(output) > 0):
+        for i in range(len(output)):
+            print(output[i])
+            columns = output[i].keys()
+            values = [output[i][column] for column in columns]
+
+            insert_statement = 'insert into attendance (%s) values %s'
+            print('SQL statment')
+            print(cursor.mogrify(insert_statement, (AsIs(','.join(columns)), tuple(values))))
+            cursor.execute(insert_statement, (AsIs(','.join(columns)), tuple(values)))
+    else:
+        print('No data')
+
+conn.commit()
+print("Closing connection")
+conn.close()
 
