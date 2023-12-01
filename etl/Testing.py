79951d0fbc3bee1b76ade86e4a14488ce9173274
Michael Lindner <Michael.Lindner@bush.edu>
2023-11-30 17:31:05 -0800

    working api call

--- etl/Testing.py
+++ etl/Testing.py
@@ -1,12 +1,31 @@
 from BbApiConnector import BbApiConnector
+import pandas as pd
 
-print('hello world')
 api_conn = BbApiConnector(r'C:\Users\Michael.Lindner\Documents\BbApiConnector-Python\resources\app_secrets.json')
 bb_session = api_conn.get_session()
 
-req = bb_session.get("https://api.sky.blackbaud.com/school/v1/levels")
+req = bb_session.get("https://api.sky.blackbaud.com/school/v1/lists/advanced/153908")
 
-print(req.encoding)
-print(req.json())
+output = req.json()['results']['rows']
 
+if (len(output) > 0):
+
+    for i in range(len(output)):
+        columns = []
+        values = []
+        for j in range(len(output[i]['columns'])):
+            columns.append(output[i]['columns'][j]['name'])
+            values.append(output[i]['columns'][j]['value'])
+
+        print(columns)
+        print(values)
+            
+        # insert_statement = 'insert into attendance (%s) values %s'
+        # print('SQL statment')
+        # print(cursor.mogrify(insert_statement, (AsIs(','.join(columns)), tuple(values))))
+
+
+
+# df = pd.json_normalize(req.json()["results"]["rows"], "columns").reset_index()
+# print(df[0:50])
 
