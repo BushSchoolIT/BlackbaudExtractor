57bb7cadbd0fb37ef3364d67dd4218a7192d98dc
Michael Lindner <Michael.Lindner@bush.edu>
2023-12-22 09:30:39 -0800

    Tidying up

--- etl/Transcripts.py
+++ etl/Transcripts.py
@@ -11,7 +11,8 @@ bb_session = api_conn.get_session()
 
 print('Getting data')
 # We need to iterate through the pages of data, BB only returns 1000 rows at a time.
-for i in range(1, 1000):
+for i in range(1, 1500):
+  print("Page " + str(i))
   req = bb_session.get("https://api.sky.blackbaud.com/school/v1/lists/advanced/153908" + "?page=" + str(i))
   df = pd.json_normalize(req.json()["results"]["rows"], "columns").reset_index()
 
@@ -33,14 +34,11 @@ for i in range(1, 1000):
           columns = df["name"][index:(index+28)].values
           values = df["value"][index:(index+28)].values
 
-          print((','.join(columns)), tuple(values))
-
           insert_statement = '''INSERT INTO transcripts (%s) VALUES %s
             ON CONFLICT (student_user_id,term_id,group_id,course_id,grade_id) 
             DO UPDATE SET 
             (student_first,student_last,grad_year,course_title,course_code,group_description,term_name,grade_description,grade_mode,grade,incomplete,score,final_score,transcript_category,required,school_year,grade_level,address_1,address_2,address_3,address_city,address_state,address_zip) = (EXCLUDED.student_first,EXCLUDED.student_last,EXCLUDED.grad_year,EXCLUDED.course_title,EXCLUDED.course_code,EXCLUDED.group_description,EXCLUDED.term_name,EXCLUDED.grade_description,EXCLUDED.grade_mode,EXCLUDED.grade,EXCLUDED.incomplete,EXCLUDED.score,EXCLUDED.final_score,EXCLUDED.transcript_category,EXCLUDED.required,EXCLUDED.school_year,EXCLUDED.grade_level,EXCLUDED.address_1,EXCLUDED.address_2,EXCLUDED.address_3,EXCLUDED.address_city,EXCLUDED.address_state,EXCLUDED.address_zip);'''
           print('SQL statment')
-          print("Iteration: " + str(index))
           print(cursor.mogrify(insert_statement, (AsIs(','.join(columns)), tuple(values))))
           cursor.execute(insert_statement, (AsIs(','.join(columns)), tuple(values)))
   else:
