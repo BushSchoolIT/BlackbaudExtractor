9c324eab7841cac871c31c6a8696e9e5d353e3a5
Michael Lindner <Michael.Lindner@bush.edu>
2024-01-22 17:40:40 -0800

    Added support for scheduled classes

--- etl/Transcripts.py
+++ etl/Transcripts.py
@@ -9,41 +9,58 @@ api_conn = BbApiConnector(path)
 print('Connecting to Blackbaud')
 bb_session = api_conn.get_session()
 
+list_IDs = ["153908", "154813", "154814", "154815", "154816", "154817", "154818", "154819",
+            "154820", "154821", "154822", "154823", "154824", "154825", "154826", "154827",
+            "154828", "154829", "154830", "154831", "154832", "154833", "154834", "154835",
+            "154836", "154837"]
+
 print('Getting data')
 # We need to iterate through the pages of data, BB only returns 1000 rows at a time.
-for i in range(1, 1500):
-  print("Page " + str(i))
-  req = bb_session.get("https://api.sky.blackbaud.com/school/v1/lists/advanced/153908" + "?page=" + str(i))
-  df = pd.json_normalize(req.json()["results"]["rows"], "columns").reset_index()
+for ID in list_IDs:
+  print(ID)
+  for i in range(1, 1500):
+    print("Page " + str(i))
+    req = bb_session.get("https://api.sky.blackbaud.com/school/v1/lists/advanced/" + ID + "?page=" + str(i))
+    
+    df = pd.json_normalize(req.json()["results"]["rows"], "columns").reset_index()
 
-  print('Connecting to postgres')
-  conn = psycopg2.connect(
-          database='postgres',
-          user='postgres',
-          password='Kto12SQL',
-          host='localhost',
-          port='5432'
-  )
+    # Giving the planned courses a grade_ID of 999999 because it is part of the PK and cannot be blank.
+    # Might want to clean out the old 999999s at the start of every year...
+    # Potentially re-write using df.loc 
+    for j in range(len(df)):
+      if (df['name'][j] == "grade_id") and (pd.isnull(df['value'][j])==True):
+        df['value'][j] = 999999
 
-  conn.autocommit = True
-  cursor = conn.cursor()
 
-  print('Inserting data')
-  if (len(df) > 0):
-      for index in range(0, len(df), 28):
-          columns = df["name"][index:(index+28)].values
-          values = df["value"][index:(index+28)].values
+    print('Connecting to postgres')
+    conn = psycopg2.connect(
+            database='postgres',
+            user='postgres',
+            password='Kto12SQL',
+            host='localhost',
+            port='5432'
+    )
 
-          insert_statement = '''INSERT INTO transcripts (%s) VALUES %s
-            ON CONFLICT (student_user_id,term_id,group_id,course_id,grade_id) 
-            DO UPDATE SET 
-            (student_first,student_last,grad_year,course_title,course_code,group_description,term_name,grade_description,grade_mode,grade,incomplete,score,final_score,transcript_category,required,school_year,grade_level,address_1,address_2,address_3,address_city,address_state,address_zip) = (EXCLUDED.student_first,EXCLUDED.student_last,EXCLUDED.grad_year,EXCLUDED.course_title,EXCLUDED.course_code,EXCLUDED.group_description,EXCLUDED.term_name,EXCLUDED.grade_description,EXCLUDED.grade_mode,EXCLUDED.grade,EXCLUDED.incomplete,EXCLUDED.score,EXCLUDED.final_score,EXCLUDED.transcript_category,EXCLUDED.required,EXCLUDED.school_year,EXCLUDED.grade_level,EXCLUDED.address_1,EXCLUDED.address_2,EXCLUDED.address_3,EXCLUDED.address_city,EXCLUDED.address_state,EXCLUDED.address_zip);'''
-          print('SQL statment')
-          print(cursor.mogrify(insert_statement, (AsIs(','.join(columns)), tuple(values))))
-          cursor.execute(insert_statement, (AsIs(','.join(columns)), tuple(values)))
-  else:
-      print('No data')
-      break
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    print('Inserting data')
+    col_count = 25
+    if (len(df) > 0):
+        for index in range(0, len(df), col_count):
+            columns = df["name"][index:(index+col_count)].values
+            values = df["value"][index:(index+col_count)].values
+
+            insert_statement = '''INSERT INTO transcripts (%s) VALUES %s
+              ON CONFLICT (student_user_id,term_id,group_id,course_id,grade_id) 
+              DO UPDATE SET 
+              (student_first,student_last,grad_year,course_title,course_code,group_description,term_name,grade_description,grade_mode,grade,score,transcript_category,school_year,grade_level,address_1,address_2,address_3,address_city,address_state,address_zip) = (EXCLUDED.student_first,EXCLUDED.student_last,EXCLUDED.grad_year,EXCLUDED.course_title,EXCLUDED.course_code,EXCLUDED.group_description,EXCLUDED.term_name,EXCLUDED.grade_description,EXCLUDED.grade_mode,EXCLUDED.grade,EXCLUDED.score,EXCLUDED.transcript_category,EXCLUDED.school_year,EXCLUDED.grade_level,EXCLUDED.address_1,EXCLUDED.address_2,EXCLUDED.address_3,EXCLUDED.address_city,EXCLUDED.address_state,EXCLUDED.address_zip);'''
+            print('SQL statment')
+            print(cursor.mogrify(insert_statement, (AsIs(','.join(columns)), tuple(values))))
+            cursor.execute(insert_statement, (AsIs(','.join(columns)), tuple(values)))
+    else:
+        print('No data')
+        break
 
 conn.commit()
 print('Closing connection')
