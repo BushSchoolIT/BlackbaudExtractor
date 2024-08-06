75b6960a7e14254ee78e01dd929755f4490a84ea
Michael Lindner <Michael.Lindner@bush.edu>
2024-08-06 12:42:58 -0700

    fixed authentification flow

---
+++ config/app_secrets_template.py
@@ -0,0 +1,6 @@
+app_id = ""
+app_secret = ""
+
+api_subscription_key = ""
+redirect_uri = "http://localhost:13631/callback"
+test_api_endpoint = "https://api.sky.blackbaud.com/school/v1/roles"
--- config/bb_auth.py
+++ config/bb_auth.py
@@ -1,18 +1,15 @@
 from bottle import route, run, template, request
 import requests
 import json
-from configparser import ConfigParser
+import app_secrets
 
-config_file_name = 'app_secrets.ini'
-config = ConfigParser()
-config.read(config_file_name)
 
 @route('/')
 def index():
     print_string = f'''
         Welcome to the Blackbaud SKY API Authorization Code Generator!
         <br><br>
-        Please go to this URL: <a href="https://oauth2.sky.blackbaud.com/authorization?client_id={config['app_secrets']['app_id']}&response_type=code&redirect_uri={config['other']['redirect_uri']}">https://oauth2.sky.blackbaud.com/authorization?client_id={config['app_secrets']['app_id']}&response_type=code&redirect_uri={config['other']['redirect_uri']}</a>
+        Please go to this URL: <a href="https://oauth2.sky.blackbaud.com/authorization?client_id={app_secrets.app_id}&response_type=code&redirect_uri={app_secrets.redirect_uri}">https://oauth2.sky.blackbaud.com/authorization?client_id={app_secrets.app_id}&response_type=code&redirect_uri={app_secrets.redirect_uri}</a>
     '''
 
     return print_string
@@ -27,10 +24,10 @@ def get_access_refresh_tokens(auth_code):
     token_uri = f'https://oauth2.sky.blackbaud.com/token'
     params = {
         'grant_type': 'authorization_code',
-        'redirect_uri': config['other']['redirect_uri'],
+        'redirect_uri': app_secrets.redirect_uri,
         'code': auth_code,
-        'client_id': config['app_secrets']['app_id'],
-        'client_secret': config['app_secrets']['app_secret']
+        'client_id': app_secrets.app_id,
+        'client_secret': app_secrets.app_secret
     }
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded'
