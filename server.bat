d20b58a03219c0cb1f47880d39cdd35048b94931
BushSchoolIT <99100584+BushSchoolIT@users.noreply.github.com>
2025-07-07 15:21:05 -0700

    Update server.bat

--- server.bat
+++ server.bat
@@ -1,2 +1,21 @@
-call C:\ProgramData\miniconda3\Scripts\activate.bat
-prefect server start
+@echo off
+setlocal enabledelayedexpansion
+
+set LOGDIR=C:\Users\Install\Logs
+set TODAY=%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%
+set LOGFILE=%LOGDIR%\prefect-server-%TODAY%.log
+
+if not exist "%LOGDIR%" (
+    mkdir "%LOGDIR%"
+)
+
+call "C:\ProgramData\miniconda3\Scripts\activate.bat"
+
+echo. >> "%LOGFILE%"
+echo ==================================================== >> "%LOGFILE%"
+echo Starting Prefect Flow at %DATE% %TIME% >> "%LOGFILE%"
+echo ==================================================== >> "%LOGFILE%"
+
+prefect server start >> "%LOGFILE%" 2>&1
+
+exit /b %ERRORLEVEL%
